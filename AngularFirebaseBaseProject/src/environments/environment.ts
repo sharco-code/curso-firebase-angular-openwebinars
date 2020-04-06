21ae5928bbd558fe177654ee261a0f966bc0b783
sharco-code <xsharco8@gmail.com>
2020-04-06 17:13:04 +0200

    configuración firebase, bootstrap y fontawesome

--- AngularFirebaseBaseProject/src/environments/environment.ts
+++ AngularFirebaseBaseProject/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDBLEdtjeyYsGrJhcXSuIOnp8ZSe9WvGJc',
+    authDomain: 'cursoangularfirebaseow.firebaseapp.com',
+    databaseURL: 'https://cursoangularfirebaseow.firebaseio.com',
+    projectId: 'cursoangularfirebaseow',
+    storageBucket: 'cursoangularfirebaseow.appspot.com',
+    messagingSenderId: '979614675568',
+   appId: '1:979614675568:web:ff9438370065d539943ab5'
+  }
 };
 
 /*
