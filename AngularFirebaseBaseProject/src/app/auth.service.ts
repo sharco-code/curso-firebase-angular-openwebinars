239b3976336f96eee371168fa8acafe9fa3ae56c
sharco-code <xsharco8@gmail.com>
2020-04-08 15:22:31 +0200

    login con google popup

--- AngularFirebaseBaseProject/src/app/auth.service.ts
+++ AngularFirebaseBaseProject/src/app/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { map } from 'rxjs/operators';
+import { auth } from 'firebase';
 
 @Injectable({
   providedIn: 'root'
@@ -8,22 +9,32 @@ import { map } from 'rxjs/operators';
 export class AuthService {
 
   constructor(public auth: AngularFireAuth) { }
+  
+  user = this.auth.authState.pipe ( map( authState => {
+    console.log('authState: ', authState);
+    if(authState) return authState;
+    else return null;
 
-    user = this.auth.authState.pipe ( map( authState => {
-      console.log('authState: ', authState);
-      if(authState) return authState;
-      else return null;
-
-    } ))
+  } ))
 
    login() {
      console.log('login');
    }
+
    glogin() {
-     console.log('google login')
+     console.log('google login');
+     this.auth.signInWithPopup( new auth.GoogleAuthProvider() )
+     .then( user => {
+       console.log('user logado: ', user);
+     })
+     .catch( error => {
+       console.log('error en google login: ', error);
+     })
    }
+
    logout() {
      console.log('logout');
+     this.auth.signOut();
    }
 
 }
